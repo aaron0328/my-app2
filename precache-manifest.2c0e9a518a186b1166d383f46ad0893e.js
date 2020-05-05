8da7233c26a4e7ffbba8ccd2ceb3dfe7b82bc84d
hei0328 <aaron0328@hotmail.com>
2020-05-05 15:06:47 +0800

    Updates

---
+++ precache-manifest.2c0e9a518a186b1166d383f46ad0893e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e89fbdbe1c71e05739bc88b8afb94ec6",
+    "url": "/my-app2/index.html"
+  },
+  {
+    "revision": "717ee5d0b5212d51605b",
+    "url": "/my-app2/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c4af435f320e694f644f",
+    "url": "/my-app2/static/js/2.3ef262bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app2/static/js/2.3ef262bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "717ee5d0b5212d51605b",
+    "url": "/my-app2/static/js/main.345c414d.chunk.js"
+  },
+  {
+    "revision": "84053cd33e7f7721f3ad",
+    "url": "/my-app2/static/js/runtime-main.f7d967c9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app2/static/media/logo.5d5d9eef.svg"
+  }
+]);
